c7265bfed3fb88b7aa8117db07d304d90a2b69bd
unknown <ej.perlazac@uea.edu.ec>
2026-02-22 22:02:37 -0300

    Sistema de Gestión de Inventarios Mejorado con archivos y manejo de excepciones

---
+++ inventario_poo_mejorado/producto.py
@@ -0,0 +1,35 @@
+class Producto:
+    def __init__(self, id_producto, nombre, cantidad, precio):
+        self.__id_producto = id_producto
+        self.__nombre = nombre
+        self.__cantidad = cantidad
+        self.__precio = precio
+
+    def get_id(self):
+        return self.__id_producto
+
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_cantidad(self):
+        return self.__cantidad
+
+    def get_precio(self):
+        return self.__precio
+
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
+
+    def set_cantidad(self, cantidad):
+        self.__cantidad = cantidad
+
+    def set_precio(self, precio):
+        self.__precio = precio
+
+    def __str__(self):
+        return (
+            f"ID: {self.__id_producto} | "
+            f"Nombre: {self.__nombre} | "
+            f"Cantidad: {self.__cantidad} | "
+            f"Precio: ${self.__precio:.2f}"
+        )
